01554f54918e3930702f8bfa8481dc46c5ba0585
Arthur-San <arthursantos242568@gmail.com>
2023-11-11 09:16:51 -0300

    ex01 organizando pastas

--- 4-data_files.py
+++ 4-data_files.py
@@ -10,5 +10,5 @@ second_created = stats.st_ctime
 
 date_created = datetime.fromtimestamp(second_created)
 # print(date_created)
-date_created_str = date_created.strftime('%Y-%m-%d/%H-%M-%S')
+date_created_str = date_created.strftime('%Y-%m-%d_%H_%M_%S')
 print(date_created_str)
---
+++ ex01.py
@@ -0,0 +1,39 @@
+# ## Organizador de Arquivos
+
+# 1. Nesse exercício vamos colocar em prática o que aprendemos nas aulas anteriores. 
+# 2. O seu desafio é criar um programa em Python que organiza arquivos de acordo com os seguintes tipos de arquivo:
+# - exe
+# - csv
+# - jpg
+# - pdf
+# - zip
+
+import os
+
+# 1 - diretório raiz do SO
+base_path = os.path.expanduser('~')
+
+# 2 - navegando no diretório Downloads
+path = os.path.join(base_path, 'Downloads')
+
+cwd = os.chdir(path)
+
+# 3 - list arquivos do diretório
+list_files = os.listdir(cwd)
+print(list_files)
+
+# 4 - criar pastas
+type_files = ['exe', 'csv', 'mkv', 'psd', 'pptx', 'pdf', 'png', 'jpeg', 'zip', 'xlsx','py', 'svg', 'fbx', 'docx', 'gif', 'sfk', 'mp4', 'htm', 'rmskin', 'msi', ]
+
+for type in type_files:
+    if type not in os.listdir():
+        os.mkdir(type)
+        
+# 5 - organizando arquivos
+for file in list_files:
+    for type in type_files:
+        if '.' + type in file:
+            old_path = os.path.join(path, file)        
+            new_path = os.path.join(path, type, file)
+            os.replace(old_path, new_path)
+
