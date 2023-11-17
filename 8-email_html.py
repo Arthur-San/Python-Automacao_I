b92361907fccc854fb82fe5106c3b220257688f0
Arthur-San <arthursantos242568@gmail.com>
2023-11-17 08:58:00 -0300

    atualização

--- 8-email_html.py
+++ 8-email_html.py
@@ -20,3 +20,25 @@ message.set_content(body, subtype="html")
 safe = ssl.create_default_context()
 
 # 2 - Adicionar anexo
+
+anexo = 'files/corpo.txt'
+# print(mimetypes.guess_type(anexo)[0])
+mimetype, mimesubtype = mimetypes.guess_type(anexo)[0].split('/')
+with open(anexo, 'rb') as a:
+    message.add_attachment(
+        a.read(),
+        maintype=mimetype,
+        subtype=mimetype,
+        filename=anexo
+    )
+    
+# 3 - Envio de email
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=safe ) as smtp:
+    smtp.login(from_email, password)
+    smtp.sendmail(
+        from_email,
+        to_email,
+        message.as_string()
+    )
+    print('email enviado!')
+    
